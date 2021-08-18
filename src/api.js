4f900b727eca9e4878b5416be12092b14c16ad40
Guillaume <guillaumegozillon@gmail.com>
2021-08-18 15:23:27 +0200

    New endpoints

--- src/api.js
+++ src/api.js
@@ -28,11 +28,16 @@ const currentDay = getCurrentDay()
 const currentYear = new Date().getFullYear()
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`
 
 // BASE URL
 const KEY_URL = `key=${process.env.REACT_APP_GAMES_API}`
 const BASE_URL = 'https://api.rawg.io/api/'
 
 const POPULAR_GAMES = `games?${KEY_URL}&date=${lastYear},${currentDate}&ordering=-rating&page_size=10`
+const UPCOMING_GAMES = `games?${KEY_URL}&date=${currentDate},${nextYear}&ordering=-added&page_size=10`
+const NEW_GAMES = `games?${KEY_URL}&date=${lastYear},${currentDate}&ordering=-released&page_size=10`
 
 export const popularGamesURL = () => `${BASE_URL}${POPULAR_GAMES}`
+export const upcomingGamesURL = () => `${BASE_URL}${UPCOMING_GAMES}`
+export const newGamesURL = () => `${BASE_URL}${NEW_GAMES}`
